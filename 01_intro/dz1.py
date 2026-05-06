52fc90a1be2cbc3ee3a357e098fbe2cd6997c342
ZabOleksandr <zoleksandr06@gmail.com>
2026-05-06 20:16:12 +0300

    dz 1 had done

---
+++ 01_intro/dz1.py
@@ -0,0 +1,24 @@
+﻿tuple1 = (1, 2, 3, 4, 5)
+tuple2 = (1, 9, 8, 2, 7)
+tuple3 = (1, 2, 4, 0, 6)
+
+#1
+print("#1 (intersection)")
+set_1 = set(tuple1) & set(tuple2) & set(tuple3)
+print(set_1)
+
+#2
+set_2_for1 = set(tuple1) - set(tuple2) - set(tuple3)
+print("#2 (difference) for 1")
+print(set_2_for1)
+set_2_for2 = set(tuple2) - set(tuple1) - set(tuple3)
+print("#2 (difference) for 2")
+print(set_2_for2)
+print("#2 (difference) for 3")
+set_2_for3 = set(tuple3) - set(tuple1) - set(tuple2)
+print(set_2_for3)
+
+#3
+print("#3 (same position in all tuples)")
+set_3 = [a for a, b, c in zip(tuple1, tuple2, tuple3) if a == b == c]
+print(set_3)
---
+++ 01_intro/tuple_example.py
@@ -0,0 +1,78 @@
+﻿# documentStatus = "In Progress", "Need Review", "Approved"
+# print(documentStatus[0])
+#
+# userTypes = ("admin", "student", "teacher")
+# user1, user2, user3 = userTypes
+# print(user1)
+# print(user2)
+# print(user3)
+#
+# user1 = "SuperAdmin"
+# userTypes2  = (user1, user2, user3)
+# print(userTypes2)
+
+userTypes3 = ("admin", "student", "teacher")
+admin, *users, teacher = userTypes3
+print(admin)
+print(users)
+print(type(users))
+print(teacher)
+
+admin, *users = userTypes3
+print(type(users))
+print(users)
+
+for userType in userTypes3:
+    print(userType)
+    print(type(userType))
+
+
+sites = ("Awc", "Wiki")
+mobileApp = ("Insta", "Facebook")
+
+allTypes = (sites, mobileApp)
+print(allTypes)
+print(type(allTypes[0]))
+
+allTypes2 = (mobileApp, sites)
+print(allTypes2)
+print(type(allTypes2[0]))
+
+concT1T2 = allTypes+allTypes2 # ( ('',''), ('',''), ('',''), ('','') )
+print(concT1T2)
+print(type(concT1T2[0]))
+
+*sitesL,  = sites
+print(sitesL)
+print(type(sitesL))
+if "Wiki" in sitesL:
+    print("Wiki is in the list")
+
+sitesA = {"Google", "X", "Wiki"}
+sitesB = {"Micro", "Lambda", "Wiki"}
+
+intersection = sitesA & sitesB
+print(intersection)
+print(type(intersection))
+
+minOne = sitesA | sitesB
+print(minOne)
+
+
+# 1. Определяем множества
+white_wool = {"Шон", "Облачко", "Зевс", "Снежок"}
+blue_eyes = {"Шон", "Облачко", "Титан", "Пират"}
+has_horns = {"Шон", "Зевс", "Титан", "Блэкки"}
+
+# 2. Практика:
+# Кто самый крутой (белый, голубоглазый и с рогами)?
+the_best = white_wool & blue_eyes & has_horns
+print(f"Самый крутой: {the_best}")  # {'Шон'}
+
+# Кто белый и голубоглазый, но БЕЗ рожек?
+cute_ones = (white_wool & blue_eyes) - has_horns
+print(f"Милашки: {cute_ones}")      # {'Облачко'}
+
+# Список всех барашков, у которых есть ХОТЯ БЫ ОДИН признак из наших кругов
+any_feature = white_wool | blue_eyes | has_horns
+print(f"Всего в кругах: {len(any_feature)} барашков")
